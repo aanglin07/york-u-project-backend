842e192b9431dfd79bf441cf4928c9ecbc67bdf9
Anthony Anglin <aanglin07@github.com>
2023-06-15 05:10:07 -0400

    added additional comments to code

--- src/filter-ratings.js
+++ src/filter-ratings.js
@@ -1,10 +1,10 @@
-//This module will find and return ratings in a range from 1-5
+//This module will find and return ratings in a range from 1-5 using a function and foreach loop
 /*The user will be given the option to choose a rating from 1-5 to view products from the lowest rated to the highest 
 by choosing from a pre-defined list .
 
 eg. If the user chooses 5, all the products with a rating of 5 will be returned*/
 
-import jerseyRatings from "./data-storage.js"
+import jerseyRatings from "./data-storage.js" //Imports from data storage module
 
 function ratingsFilter(a){
 jerseyRatings.filter(i=>i.rating == a).forEach(result => console.log(result))
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 console.log("Hello World")
 
-import ratingsFilter from './filter-ratings.js';
+import ratingsFilter from './filter-ratings.js'; //Imports from module, filter-ratings.
 
 
 
-console.log(ratingsFilter(4));
+console.log(ratingsFilter()); //Enter a value from 1 to 5 to return the desired product with that specifi rating

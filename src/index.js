1090c9a5dd1c00568ee0e8de76b56871647030e0
Anthony Anglin <aanglin07@github.com>
2023-06-15 05:12:59 -0400

    added additional comments to code

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import ratingsFilter from './filter-ratings.js'; //Imports from module, filter-r
 
 
 
-console.log(ratingsFilter()); //Enter a value from 1 to 5 to return the desired product with that specifi rating
+console.log(ratingsFilter()); //Enter a value from 1 to 5 to return the desired product with that specific rating

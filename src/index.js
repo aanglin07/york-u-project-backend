a93f4274722c0f4474863156cc63fb0e95a4f68f
aanglin07 <aanglin07@gmail.com>
2023-06-22 03:05:27 -0400

    added tests

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import ratingsFilter from './filter-ratings.js'; //Imports from module, filter-r
 
 
 
-console.log(ratingsFilter(5)); //Enter a value from 1 to 5 to return the desired product with that specific rating
+console.log(ratingsFilter(6)); //Enter a value from 1 to 5 to return the desired product with that specific rating
---
+++ src/module.spec.js
@@ -0,0 +1,28 @@
+import getRated from "./getRatings";
+import ratingsFilter from "./filter-ratings";
+
+
+
+describe("Ability to search array for products with a rating", () => {
+
+    
+    test ("should return all products with a rating", () => {
+            const result = getRated()
+            console.log(result)
+      })
+    })
+    test("should return products that have a specific rating", () => {
+
+        const result = ratingsFilter(5)
+        console.log(result)
+    })
+
+    test("should return null for any request outside the range of 1 to 5", () => {
+
+        const result = ratingsFilter(6)
+        console.log(result)
+        expect(result).toBe(null)
+    })
+
+    
+

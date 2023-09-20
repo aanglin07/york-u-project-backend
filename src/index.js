e605b2300e8a34c5fba9c3ba63d6e860111f0c09
aanglin07 <aanglin07@gmail.com>
2023-09-20 03:58:38 -0400

    Set up of API server

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
+import express from 'express'
 import ratingsFilter from './filter-ratings.js'; //Imports from module, filter-ratings.
+const app = express()
+const port = 3000
+// allows us to parse json 
+app.use(express.json())
+
+app.get('/', (req, res) => res.send('Hello world'))
+
+app.listen(port, () => console.log(`API server ready on http://localhost:${port}`))
 
 
 
 
-console.log(ratingsFilter(6)); //Enter a value from 1 to 5 to return the desired product with that specific rating
+
+
+console.log(ratingsFilter(3)); //Enter a value from 1 to 5 to return the desired product with that specific rating

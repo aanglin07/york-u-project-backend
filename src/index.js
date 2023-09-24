c7a331f58b34ed34ab8fd1b839cd3250e8de36f4
aanglin07 <aanglin07@gmail.com>
2023-09-23 22:22:57 -0400

    changed path for Data Routes and dotenv config

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
 import express from 'express'
-import jerseyDataRoutes from './jerseyData.js'
-import teamDataRoutes from './teamData.js'
-import leagueDataRoutes from './leagueData.js'
-import ratingsFilter from './filter-ratings.js'; //Imports from module, filter-ratings.
+import jerseyDataRoutes from '../src/Routes/jerseyData.js'
+import teamDataRoutes from '../src/Routes/teamData.js'
+import leagueDataRoutes from '../src/Routes/leagueData.js'
+import dotenv from 'dotenv'
+dotenv.config()
+//import ratingsFilter from './filter-ratings.js'; //Imports from module, filter-ratings.
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 3000
+
 // allows us to parse json 
 app.use(express.json())
 
-app.get('/', (req, res) => res.send('Hello world'))
+app.get('/', (req, res) => res.send('Welcome to the Jersey App'))
 app.use('/jersey', jerseyDataRoutes )
 app.use('/teams', teamDataRoutes )
 app.use('/leagues', leagueDataRoutes )
 
-app.listen(port, () => console.log(`API server ready on http://localhost:${port}`))
+app.listen(PORT, () => console.log(`API server ready on http://localhost:${PORT}`))
 
 
 
 
 
 
-console.log(ratingsFilter(3)); //Enter a value from 1 to 5 to return the desired product with that specific rating
+//console.log(ratingsFilter(3)); //Enter a value from 1 to 5 to return the desired product with that specific rating

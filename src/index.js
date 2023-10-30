21085b8d128dfad1590c9ec134ef0d3be7eae237
aanglin07 <aanglin07@gmail.com>
2023-10-30 05:46:10 -0400

    moved routes to their appropriate positions

--- src/index.js
+++ src/index.js
@@ -6,20 +6,32 @@ import dotenv from 'dotenv'
 import jsonwebtoken from 'jsonwebtoken'
 import {expressjwt as jwt} from 'express-jwt'
 import argon2 from 'argon2'
+import { jerseyData, users } from './data-storage.js'
 dotenv.config()
 //import ratingsFilter from './filter-ratings.js'; //Imports from module, filter-ratings.
 const app = express()
 const PORT = process.env.PORT
-const users = [{ username: "testing@test.com", password: "$argon2i$v=19$m=16,t=2,p=1$WDFaSWQybFozbWtqekdwYQ$MoHaBhES1PAFaPjcjP3jbA"}]
+
 
 // allows us to parse json 
 app.use(express.json())
+
+
+app.get('/', (req, res) => res.send(jerseyData)) //Gets list of all jerseys in the database
+
+
+
+app.use('/jersey', jerseyDataRoutes )
+app.use('/teams', teamDataRoutes )
+app.use('/leagues', leagueDataRoutes )
 
 app.post('/login', async (req, res) => {
     const reqUser = req.body.username
     const password = req.body.password
     const userFound = users.find( ({ username }) => username === reqUser)
 
+    
+    
     if (userFound && await argon2.verify(userFound.password, password)) {
         const token = jsonwebtoken.sign({reqUser}, process.env.JWT_SECRET, {expiresIn: '1m'}) 
         return res.json({token}) 
@@ -28,16 +40,6 @@ app.post('/login', async (req, res) => {
 })
 
 app.use( jwt({secret: process.env.JWT_SECRET, algorithms: ['HS256']}) )
-
-app.get('/protected', (req, res) => {
-    res.json({message: "You made it!", user: req.user}) 
-})
-
-app.get('/', (req, res) => res.send('Welcome to the Jersey App'))
-app.use('/jersey', jerseyDataRoutes )
-app.use('/teams', teamDataRoutes )
-app.use('/leagues', leagueDataRoutes )
-
 app.listen(PORT, () => console.log(`API server ready on http://localhost:${PORT}`))
 
 

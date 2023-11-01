1d7008b731e842472f3094459d22c6fa9d1d2e11
aanglin07 <aanglin07@gmail.com>
2023-11-01 00:25:49 -0400

    created file to execute migrating sql commands

---
+++ migrate.js
@@ -0,0 +1,24 @@
+import fs from 'fs/promises'
+import path from 'path'
+import { db } from "./src/models/dbconnection.js";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const migrate = async () => {
+    const migrations = await fs.readdir(path.join(__dirname, 'migrations'));
+
+    for (const migration of migrations) {
+        const fileContent = await fs.readFile(path.join(__dirname, 'migrations', migration), 'utf-8');
+        await db.query(fileContent);
+    }
+}
+
+migrate().then(() => {
+    console.log('Migrations done!')
+    process.exit(0)
+}).catch(err => {
+    console.error(err)
+    process.exit(1)
+})

2524ea9e2ab421e88038c06324a59cd0c311a53c
aanglin07 <aanglin07@gmail.com>
2023-11-17 21:00:13 -0500

    added create table script

--- migrations/004-jersey.sql
+++ migrations/004-jersey.sql
@@ -1,2 +1,11 @@
-
+CREATE TABLE IF NOT EXISTS `jersey` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `img` varchar(255) NOT NULL,
+  `team_kit` varchar(255) NOT NULL,
+  `Year` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `league_name` varchar(255) NOT NULL,
+  `purchase_link` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+)
 
